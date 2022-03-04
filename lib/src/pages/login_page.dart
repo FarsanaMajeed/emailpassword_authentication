4c2e6eb94bb3b23086738f50e33acaa8508aa964
FarsanaMajeed <farsanamajeed786@gmail.com>
2022-03-04 21:49:24 +0530

    Authentication using email and password

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -1,5 +1,3 @@
-
-
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
@@ -7,15 +5,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:loginform_new/bloc/auth_cubit.dart';
 import 'package:loginform_new/src/pages/dashboard_page.dart';
+import 'package:loginform_new/src/pages/reg_form.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
+  _LoginPageState createState() => _LoginPageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _LoginPageState extends State<LoginPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   GlobalKey<FormState> formKey = GlobalKey();
@@ -24,99 +23,108 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(children: [
+      backgroundColor: Colors.white,
+        body: Column(
+      children: [
+       SizedBox(height: 12),
         Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SafeArea(
-            child: Form(
-                key: formKey,
-                child: Column(
-                  children: [
-                    SizedBox(height: 8),
-                    TextFormField(
-                      validator: (val) {
-                        String data = val ?? '';
-                        if (data.trim().isEmpty) {
-                          return ' Please enter email';
-                        }
-                      },
-                      controller: emailController,
-                      keyboardType: TextInputType.emailAddress,
-
-                      decoration: InputDecoration(
-                          labelText: "Enter email",
-                          contentPadding: EdgeInsets.all(4),
-                          border: OutlineInputBorder()),
-                    ),
-                    Divider(thickness: 4),
-                    TextFormField(
-                      autovalidateMode: AutovalidateMode.onUserInteraction,
-                      validator: (val) {
-                        String data = val ?? '';
-                        if (data.trim().isEmpty) {
-                          return ' Please enter password';
-                        } else if (data.length > 6) {
-                          return "password must be 6 length";
-                        }
-                      },
-                      controller: passwordController,
-                      obscureText: obscureText,
-                      decoration: InputDecoration(
-                        labelText: "password",
-                        suffix: obscureText
-                            ? IconButton(
-                            onPressed: () {
-                              setState(() {
-                                obscureText = false;
-                              });
-                            },
-                            icon: Icon(Icons.visibility))
-                            : IconButton(
-                            onPressed: () {
-                              setState(() {
-                                obscureText = true;
-                              });
-                            },
-                            icon: Icon(Icons.visibility_off)),
-                        contentPadding: EdgeInsets.all(4),
-                        border: OutlineInputBorder(),
-                      ),
-                    ),
-                    //here gives used cubit and state name
-                    //BlockConsumer used instead of BlockBuilder and BlockBuilder
-                    BlocConsumer<AuthCubit, AuthState>(
-                        listener: (context, state) {
-                          if(state is AuthSuccess){
-                            Navigator.push(context, MaterialPageRoute(builder:(_)=> DashboardPage()));
-                          }else if(state is AuthError){
-                            Fluttertoast.showToast(msg: "Login failed");
-                          }else{
-
-                          }
-                        },
-                        builder: (context, state) {
-                          //the value 0f the state variable equal to the state emit from the cubit
-                          //ElevatedButton wrap with BlockBuilder for when login click the ElevatedButton changed for an loader
-                          if (state is AuthLoading) {
-                            return const CircularProgressIndicator();
-                          } else {
-                            return ElevatedButton(
-                              onPressed: () {
-                                String email = emailController.text.trim();
-                                String password = passwordController.text.trim();
-                                BlocProvider.of<AuthCubit>(context)
-                                    .signInUserEmailPassword(email, password);
-                              },
-                              child: const Text('login'),
-                            );
-                          }
-                        }),
-                  ],
-                )),
+          padding:EdgeInsets.only(top: 100,left: 10,right: 10),
+          child: TextFormField(
+          validator: (val) {
+            String data = val ?? '';
+            if (data.trim().isEmpty) {
+              return ' Please enter email';
+            }
+          },
+          controller: emailController,
+          keyboardType: TextInputType.emailAddress,
+          decoration: InputDecoration(
+              labelText: "Enter email",
+              contentPadding: EdgeInsets.all(4),
+              border: OutlineInputBorder()),
+        ),
+        ),
+        Padding(
+          padding:EdgeInsets.only(top: 20,left: 10,right: 10),
+          child: TextFormField(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          validator: (val) {
+            String data = val ?? '';
+            if (data.trim().isEmpty) {
+              return ' Please enter password';
+            } else if (data.length < 8) {
+              return "password must be 8 length";
+            }
+          },
+          controller: passwordController,
+          obscureText: obscureText,
+          decoration: InputDecoration(
+            labelText: "password",
+            suffix: obscureText
+                ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        obscureText = false;
+                      });
+                    },
+                    icon: Icon(Icons.visibility))
+                : IconButton(
+                    onPressed: () {
+                      setState(() {
+                        obscureText = true;
+                      });
+                    },
+                    icon: Icon(Icons.visibility_off)),
+            contentPadding: EdgeInsets.all(4),
+            border: OutlineInputBorder(),
           ),
         ),
-      ]),
+        ),
+        BlocConsumer<AuthCubit, AuthState>(listener: (context, state) {
+          if (state is AuthSuccess) {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (_) => DashboardPage()));
+          } else if (state is AuthError) {
+            Fluttertoast.showToast(msg: "Login failed");
+          } else {}
+        }, builder: (context, state) {
+          if (state is AuthLoading) {
+            return const CircularProgressIndicator();
+          } else {
+            return Padding(
+              padding: const EdgeInsets.only(top:10),
+              child: ElevatedButton(
+                onPressed: () {
+                  String email = emailController.text.trim();
+                  String password = passwordController.text.trim();
+                  BlocProvider.of<AuthCubit>(context)
+                      .signInUserEmailPassword(email, password);
+                },
+                child: const Text('login'),
+              ),
+            );
+          }
+        }),
+       InkWell(
+         onTap: (){
+           Navigator.push(context,MaterialPageRoute(builder:(_)=> RegistrationPage()));
+         },
+           child: Padding(
+          padding:EdgeInsets.all(10),
+          child:Center(
+            child: Row(
+              children: [
+                Padding(
 
-    );
+                  padding: const EdgeInsets.only(left:90),
+                  child: Text('create an account ?',style: TextStyle(color: Colors.blue,fontSize: 18),),
+                ),
+              ],
+            ),
+          )
+        )
+       ),
+      ],
+    ));
   }
 }
--- lib/src/pages/reg_form.dart
+++ lib/src/pages/reg_form.dart
@@ -4,18 +4,18 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:loginform_new/bloc/auth_cubit.dart';
 import 'package:loginform_new/src/pages/dashboard_page.dart';
 
-class FormPage extends StatefulWidget {
-  const FormPage({Key? key}) : super(key: key);
+class RegistrationPage extends StatefulWidget {
+  const RegistrationPage({Key? key}) : super(key: key);
 
   @override
-  _FormPageState createState() => _FormPageState();
+  _RegistrationPageState createState() => _RegistrationPageState();
 }
 
-class _FormPageState extends State<FormPage> {
+class _RegistrationPageState extends State<RegistrationPage> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController confirmPasswordController = TextEditingController();
-  GlobalKey<FormState> formState = GlobalKey();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   bool obscureText = true;
   @override
@@ -27,189 +27,183 @@ class _FormPageState extends State<FormPage> {
           textAlign: TextAlign.center,
         ),
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListView(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 15),
-                child: TextFormField(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 15, left: 10, right: 10),
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.person),
+                        labelText: " Full Name",
+                        contentPadding: EdgeInsets.all(8),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
+                    validator: (String? value) {
+                      if (value!.isEmpty) {
+                        return "Please enter name";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15),
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.phone),
+                      labelText: "Enter phone number",
+
+                      hintText: '0000000000',
+                      contentPadding: EdgeInsets.all(8),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    maxLength: 10,
+                  ),
+                ),
+                TextFormField(
+                  validator: (val) {
+                    if (val!.isEmpty) {
+                      return 'Please enter email';
+                    }
+                    if (!RegExp("^[a-zA-ZO-9+_.-]+@[a-zA-ZO-9.-]+.[a-z]")
+                        .hasMatch(val)) {
+                      return "Please enter a valid email";
+                    }
+                    return null;
+                  },
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
-                      icon: Icon(Icons.person),
-                      labelText: "Name",
+
+                      labelText: "Enter email",
+                      hintText: "email@gmail.com",
+                      prefixIcon:Icon(Icons.mail),
                       contentPadding: EdgeInsets.all(8),
                       border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10))),
+                  maxLength: 25,
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 15),
-                child: TextField(
-                  keyboardType: TextInputType.number,
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.phone),
-                    labelText: "Enter phone number",
-                    hintText: '+91 0000000000',
-                    contentPadding: EdgeInsets.all(8),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                  ),
-                  maxLength: 10,
-                ),
-              ),
-              TextFormField(
-                validator: (val) {
-                  String data = val ?? '';
-                  if (data.trim().isEmpty) {
-                    return 'please enter email';
-                  } else if (data.length < 5) {
-                  } else {
-                    return null;
-                  }
-                },
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    icon: Icon(Icons.mail),
-                    labelText: "Enter email",
-                    hintText: "email@gmail.com",
-                    contentPadding: EdgeInsets.all(8),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10))),
-                maxLength: 25,
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (val) {
-                  String data = val ?? '';
-                  if (data.trim().isEmpty) {
-                    return 'please enter password';
-                  } else if (data.length < 8) {
-                    return 'please enter 8 character long';
-                  }
-                },
-                controller: passwordController,
-                keyboardType: TextInputType.emailAddress,
-                obscureText: obscureText,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.security),
-                  hintText: 'Enter password',
-                  labelText: "Enter password",
-                  suffix: obscureText
-                      ? IconButton(
-                      onPressed: () {
-                        setState(() {
-                          obscureText = false;
-                        });
-                      },
-                      icon: Icon(Icons.visibility))
-                      : IconButton(
-                      onPressed: () {
-                        setState(() {
-                          obscureText = true;
-                        });
-                      },
-                      icon: Icon(Icons.visibility_off)),
-                  contentPadding: EdgeInsets.all(4),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 15),
-                child: TextFormField(
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                SizedBox(height: 10),
+                TextFormField(
                   validator: (val) {
                     String data = val ?? '';
-                    if (data.trim().isEmpty) {
-                      return 'please enter password';
-                    } else if (passwordController.text !=
-                        confirmPasswordController.text) {
-                      return "password not match please re-enter";
+                    if (data.isEmpty) {
+                      return 'Please enter password';
+                    } else if (data.length < 8) {
+                      return 'Password length should not be less than 8 character';
                     } else {
                       return null;
                     }
                   },
-                  controller: confirmPasswordController,
-                  keyboardType: TextInputType.emailAddress,
+                  controller: passwordController,
+                  keyboardType: TextInputType.visiblePassword,
                   obscureText: obscureText,
                   decoration: InputDecoration(
-                    icon: Icon(Icons.security),
-                    hintText: 'Confirm password',
-                    labelText: "Re-Enter password",
+                    labelText: "Enter password",
+                    prefixIcon: Icon(Icons.security),
                     suffix: obscureText
                         ? IconButton(
-                        onPressed: () {
-                          setState(() {
-                            obscureText = false;
-                          });
-                        },
-                        icon: Icon(Icons.visibility))
+                            onPressed: () {
+                              setState(() {
+                                obscureText = false;
+                              });
+                            },
+                            icon: Icon(Icons.visibility))
                         : IconButton(
-                        onPressed: () {
-                          setState(() {
-                            obscureText = true;
-                          });
-                        },
-                        icon: Icon(Icons.visibility_off)),
+                            onPressed: () {
+                              setState(() {
+                                obscureText = true;
+                              });
+                            },
+                            icon: Icon(Icons.visibility_off)),
                     contentPadding: EdgeInsets.all(4),
                     border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10)),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 15),
-                child: Center(
-                  widthFactor: 50,
-                  child: BlocConsumer<AuthCubit, AuthState>(
-                    listener :(context,state){
-                      if(state is AuthSuccess){
-                        Navigator.push(context, MaterialPageRoute(builder: (_)=>DashboardPage()));
-                      }else if(state is AuthError){
-                        Fluttertoast.showToast(msg: 'Registration failed');
-                      }
-                    },
-                    builder: (context, state) {
-                      if (state is AuthLoading) {
-                        return CircularProgressIndicator();
+                Padding(
+                  padding: const EdgeInsets.only(top: 15),
+                  child: TextFormField(
+                    validator: (val) {
+                      String data = val ?? '';
+                      if (data.isEmpty) {
+                        return 'please enter password';
+                      } else if (passwordController.text !=
+                          confirmPasswordController.text) {
+                        return "password doesn't match";
                       } else {
-                        return ElevatedButton(
-                          onPressed: () {
-                            if (!formState.currentState!.validate()) {
-                              return;
-
-                            }
-                            String email=emailController.text.trim();
-                            String password=passwordController.text.trim();
-                            BlocProvider.of<AuthCubit>(context).registerUserEmailPassword(email,password);
-                          },
-                          child: Text("login"),
-                        );
+                        return null;
                       }
                     },
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 5),
-                child: Center(
-                  widthFactor: 50,
-                  child: TextButton(
-                    onPressed: () {
-                      if (!formState.currentState!.validate()) {
-                        return;
-                      }
-                    },
-                    child: Text(
-                      'Forgot Password ?',
-                      style: TextStyle(color: Colors.blue, fontSize: 13),
+                    controller: confirmPasswordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: obscureText,
+                    decoration: InputDecoration(
+                      labelText: "confirm password",
+                      prefixIcon: Icon(Icons.security),
+                      suffix: obscureText
+                          ? IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  obscureText = false;
+                                });
+                              },
+                              icon: Icon(Icons.visibility))
+                          : IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  obscureText = true;
+                                });
+                              },
+                              icon: Icon(Icons.visibility_off)),
+                      contentPadding: EdgeInsets.all(4),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
                     ),
                   ),
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 15),
+                  child: Center(
+                    widthFactor: 50,
+                    child: BlocConsumer<AuthCubit, AuthState>(
+                      listener: (context, state) {
+                        if (state is AuthSuccess) {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) => DashboardPage()));
+                        } else if (state is AuthError) {
+                          Fluttertoast.showToast(msg: 'Registration failed');
+                        }
+                      },
+                      builder: (context, state) {
+                        if (state is AuthLoading) {
+                          return CircularProgressIndicator();
+                        } else {
+                          return ElevatedButton(
+                            onPressed: () {
+                              String email = emailController.text.trim();
+                              String password = passwordController.text.trim();
+                              BlocProvider.of<AuthCubit>(context)
+                                  .registerUserEmailPassword(email, password);
+                            },
+                            child: Text("Register"),
+                          );
+                        }
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
